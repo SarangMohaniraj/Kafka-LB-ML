5eafa9e353c5ca0094727b93785aa93ae999fdae
Varsha C L <clvarsha1998@gmail.com>
2024-04-19 17:48:28 -0500

    chore: change cpu usage logic

--- load_predict.py
+++ load_predict.py
@@ -2,6 +2,7 @@ import os
 import joblib
 import pandas as pd
 
+
 def load_model(load_path):
     try:
         model = joblib.load(load_path)
@@ -10,20 +11,37 @@ def load_model(load_path):
     except Exception as e:
         print(e)
 
+
 def create_dataframe(data):
     data = {
-        'cpu_usage': float(data["java.lang:type=OperatingSystem"]["ProcessCpuLoad"]),
-        'memory_usage' : float(data["java.lang:type=Memory"]["HeapMemoryUsage"]["used"]) ,
-        'time_duration': (float(data["kafka.network:type=RequestMetrics,name=TotalTimeMs,request=Produce"]["Count"])+ float(data["kafka.network:type=RequestMetrics,name=TotalTimeMs,request=FetchConsumer"]["Count"]))/1000, #time is in ms
-        'memory_accesses_per_instruction': float(data["kafka.server:type=BrokerTopicMetrics,name=MessagesInPerSec"]["Count"])
-        }
+        "cpu_usage": float(data["java.lang:type=OperatingSystem"]["SystemLoadAverage"])
+        / float(data["java.lang:type=OperatingSystem"]["AvailableProcessors"]),
+        "memory_usage": float(data["java.lang:type=Memory"]["HeapMemoryUsage"]["used"]),
+        "time_duration": (
+            float(
+                data[
+                    "kafka.network:type=RequestMetrics,name=TotalTimeMs,request=Produce"
+                ]["Count"]
+            )
+            + float(
+                data[
+                    "kafka.network:type=RequestMetrics,name=TotalTimeMs,request=FetchConsumer"
+                ]["Count"]
+            )
+        )
+        / 1000,  # time is in ms
+        "memory_accesses_per_instruction": float(
+            data["kafka.server:type=BrokerTopicMetrics,name=MessagesInPerSec"]["Count"]
+        ),
+    }
     dataframe = pd.DataFrame(data, index=[0])
     return dataframe
 
-def predict_load_state(data, datadir ="./"):
-    #Loading the saved model
-    model = load_model(load_path = f'{datadir}trained_model.pkl')
-    dataframe =create_dataframe(data)
+
+def predict_load_state(data, datadir="./"):
+    # Loading the saved model
+    model = load_model(load_path=f"{datadir}trained_model.pkl")
+    dataframe = create_dataframe(data)
     prediction = model.predict(dataframe)
     print(f"prediction {prediction}")
     return prediction
