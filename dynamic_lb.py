28f6ad2938a08e8f491766e814ca2b30e8d3040a
swatigk <51986997+swatigk@users.noreply.github.com>
2024-04-19 16:22:00 -0500

    Update dynamic_lb.py: add predict_load_state

--- dynamic_lb.py
+++ dynamic_lb.py
@@ -1,7 +1,10 @@
 from metrics import get_kafka_metrics
+from load_predict import predict_load_state
 
 def dynamic_load_balancing():
    kafka_metrics = get_kafka_metrics("example_topic")
    print("kafka_metrics", kafka_metrics)
+   load_state = predict_load_state(kafka_metrics)
+   print("load_state", load_state)
 
 dynamic_load_balancing()
