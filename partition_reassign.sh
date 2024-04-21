9c4b497d90b85d80cd8043001ac10ee4f7182d0d
Swati K <swatigopikrishna@gmail.com>
2024-04-21 18:36:38 -0500

    fix: Multiple topics for partition reassign

--- partition_reassign.sh
+++ partition_reassign.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <topic_name> <broker_list> <bootstrap_server>"
+    echo "Usage: $0 <topic_name_list> <broker_list> <bootstrap_server>"
     exit 1
 fi
 
-topic_name=$1
+topic_names=$1
 broker_list=$2
 bootstrap_server=$3
 
+IFS=',' read -ra topics <<< "$topic_names"
+
 echo '{
-  "topics": [
-    {"topic": "'$topic_name'"}
-  ],
+  "topics": [' > change_topics.json
+
+for topic in "${topics[@]}"; do
+    echo '    {"topic": "'"$topic"'"},' >> change_topics.json
+done
+
+echo '  ],
   "version": 1
-}' > change_topics.json
+}' >> change_topics.json
+
+# Remove trailing comma
+sed -i '$s/,$//' change_topics.json
 
 output=$(/bin/kafka-reassign-partitions --bootstrap-server $bootstrap_server \
                                         --topics-to-move-json-file change_topics.json \
